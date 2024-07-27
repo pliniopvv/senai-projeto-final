7d048f73e955f48c6d0030c2e3d23fd014eb134c
Plínio Pvv <gangss2@hotmail.com>
2024-07-27 12:50:36 -0300

    +spring

---
+++ src/main/java/br/com/pvv/senai/SenaiProjetoFinalApplication.java
@@ -0,0 +1,13 @@
+package br.com.pvv.senai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenaiProjetoFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenaiProjetoFinalApplication.class, args);
+	}
+
+}
